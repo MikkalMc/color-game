76186ab70ca95f83497e1d9ceb3a036855dfdf31
Mikkal McNulty <mcnultymikkal@gmail.com>
2017-07-07 15:02:15 -0700

    make HTML markup, style some, js rough draft

---
+++ src/app.js
@@ -0,0 +1,13 @@
+function randomRGB() {
+	 return Math.floor(Math.random() * (255 - 0) + 0);
+}
+
+function setColors() {
+	var randRed  = randomRGB();
+	var randGreen = randomRGB();
+	var randBlue = randomRGB();
+	var setRGB = "(" + randRed + "," + randGreen + "," + randBlue + ")";
+	return setRGB;	
+}
+
+document.getElementById("rgb-text")

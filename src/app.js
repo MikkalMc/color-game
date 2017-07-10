1010938357e78ed2db3ccfc79aa486aa25ba8b28
Mikkal McNulty <mcnultymikkal@gmail.com>
2017-07-09 19:08:25 -0700

    create a 'key' function generator and variable to make an 'answer'

--- src/app.js
+++ src/app.js
@@ -9,10 +9,27 @@ function setColors() {
     var setRGB = "(" + randRed + "," + randGreen + "," + randBlue + ")";
     return setRGB;
 }
+//set game mode hard/easy
+//** IN PROGRESS
+var hardMode = true;
+
+//define 'key' to be used to identify which square is the correct one
+var key;
+
+function createKey(gameMode) {
+	if(hardMode) {
+		key = Math.floor(Math.random() * (7 - 0));
+	} else {
+		key = Math.floor(Math.random() * (4 - 0));
+	}
+}
+createKey();
+console.log(key);
 
 //global 'answer' RGB
 var globalRGB = setColors();
 
+//append globalRGB to the title
 function displayRGB() {
     document.getElementById("rgb-text").innerHTML += globalRGB;
 }
@@ -21,14 +38,25 @@ function displayRGB() {
 document.getElementById("rgb-text").addEventListener("load", displayRGB());
 
 var squares = document.getElementsByClassName("square");
+// var squareList;
 
 function setSquareRGB() {
     for (i = 0; i < squares.length; i++) {
         var fetchColors = setColors();
         var squareRand = 'rgb' + fetchColors;
-        console.log(squareRand);
+        // console.log(squareRand);
+        if(i === key) {
+        	squares[i].style.backgroundColor = globalRGB;
+        } else {
         squares[i].style.backgroundColor = squareRand;
+        }
     }
 }
 setSquareRGB();
-console.log(globalRGB);
+
+// function assignChosenOne() {
+// 	for (i = 0; i < squares.length; i++) {
+		
+// 	}
+// }
+

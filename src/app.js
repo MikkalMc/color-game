2429a9385b49011a5f99f2c5042b8d2492ba695f
Mikkal McNulty <mcnultymikkal@gmail.com>
2017-07-09 18:41:19 -0700

    create a global 'answer' RGB variable to make things easier

--- src/app.js
+++ src/app.js
@@ -1,30 +1,34 @@
 function randomRGB() {
-	 return Math.floor(Math.random() * (255 - 0) + 0);
+    return Math.floor(Math.random() * (255 - 0) + 0);
 }
 
 function setColors() {
-	var randRed  = randomRGB();
-	var randGreen = randomRGB();
-	var randBlue = randomRGB();
-	var setRGB = "(" + randRed + "," + randGreen + "," + randBlue + ")";
-	return setRGB;	
+    var randRed = randomRGB();
+    var randGreen = randomRGB();
+    var randBlue = randomRGB();
+    var setRGB = "(" + randRed + "," + randGreen + "," + randBlue + ")";
+    return setRGB;
 }
 
+//global 'answer' RGB
+var globalRGB = setColors();
+
 function displayRGB() {
-	var myRGB = setColors();
-	document.getElementById("rgb-text").innerHTML += myRGB;
+    document.getElementById("rgb-text").innerHTML += globalRGB;
 }
 
 //on load, change the rgb being displayed on title.
 document.getElementById("rgb-text").addEventListener("load", displayRGB());
 
 var squares = document.getElementsByClassName("square");
+
 function setSquareRGB() {
-	for (i=0; i<squares.length; i++) {
-		var fetchColors = setColors();
-		var squareRand = 'rgb' + fetchColors; 
-		console.log(squareRand);
-		squares[i].style.backgroundColor = squareRand;
-	}
+    for (i = 0; i < squares.length; i++) {
+        var fetchColors = setColors();
+        var squareRand = 'rgb' + fetchColors;
+        console.log(squareRand);
+        squares[i].style.backgroundColor = squareRand;
+    }
 }
 setSquareRGB();
+console.log(globalRGB);

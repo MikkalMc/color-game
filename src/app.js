3fac4e50b540616feb7de24e6c484cc5f8a20706
Mikkal McNulty <mcnultymikkal@gmail.com>
2017-07-09 18:36:49 -0700

    append random rgb styling to the squares

--- src/app.js
+++ src/app.js
@@ -15,5 +15,16 @@ function displayRGB() {
 	document.getElementById("rgb-text").innerHTML += myRGB;
 }
 
+//on load, change the rgb being displayed on title.
 document.getElementById("rgb-text").addEventListener("load", displayRGB());
-//change.
+
+var squares = document.getElementsByClassName("square");
+function setSquareRGB() {
+	for (i=0; i<squares.length; i++) {
+		var fetchColors = setColors();
+		var squareRand = 'rgb' + fetchColors; 
+		console.log(squareRand);
+		squares[i].style.backgroundColor = squareRand;
+	}
+}
+setSquareRGB();
